8cf5e0e1b319721da3cf930a5bf4dd104913afc1
rezamf17 <rezayuyara@gmail.com>
2021-05-11 19:57:11 +0700

    memanggil routes

--- server.js
+++ server.js
@@ -6,6 +6,11 @@ const app = express()
 //server
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
+
+//memanggil routes
+const routes = require('./routes')
+routes(app)
+
 app.listen(3000, () => {
     console.log(`Server started on port`);
 });
